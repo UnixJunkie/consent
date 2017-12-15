7fb015bfcdb875c4b15e7232706c64ae509860f6
Francois BERENGER <ligand@free.fr>
2017-12-15 10:41:03 +0900

    added -p option to sdf_get

--- src/sdf_get.ml
+++ src/sdf_get.ml
@@ -3,43 +3,72 @@
 
 open Printf
 
+module Ht = Hashtbl
+module StringSet = BatSet.String
+
 let main () =
   Log.set_log_level Log.INFO;
-  Log.set_output stderr;
   Log.color_on ();
-  (* mandatory options *)
-  let input_fn = ref "" in
-  let names = ref "" in
-  let usage_message =
-    sprintf "usage:\n%s -i molecules.sdf -names \"mol1,mol2,mol10\"\n"
-      Sys.argv.(0) in
-  let argc = Array.length Sys.argv in
+  (* options *)
+  let argc, args = CLI.init () in
   if argc = 1 then
-    let () = eprintf "%s" usage_message in
-    let _ = exit 1 in
-    () (* for typing *)
-  else
-    Arg.parse
-      ["-i", Arg.Set_string input_fn,
-       "<filename> where to read molecules from";
-       "-names", Arg.Set_string names,
-       "name1[,name2[,...]] which molecules to get"]
-      (fun arg -> raise (Arg.Bad ("Bad argument: " ^ arg)))
-      usage_message;
-  let names = BatString.nsplit !names ~by:"," in
+    (eprintf "usage:\n\
+              %s -i molecules.sdf [-p] \
+              {-names \"mol1,mol2,mol10\"|-n names_file}\n\
+              -i <file.sdf>: where to read molecules from\n\
+              -names name1,name2,name3: name of molecules to get\n\
+              -n <names_file>: names of molecules to get, one per line\n\
+              -p: preserve (names) order when writting molecules out\n"
+       Sys.argv.(0);
+     exit 1);
+  let input_fn = CLI.get_string ["-i"] args in
+  let names = match CLI.get_string_opt ["-names"] args with
+    | None -> ""
+    | Some ns -> ns in
+  let names_fn = match CLI.get_string_opt ["-n"] args with
+    | None -> ""
+    | Some fn -> fn in
+  let preserve_order = CLI.get_set_bool ["-p"] args in
+  let selected_names =
+    (* -names and -n are incompatible but one is mandatory *)
+    assert(names <> "" || names_fn <> "");
+    assert(names = "" || names_fn = "");
+    if names <> "" then
+      BatString.nsplit ~by:"," names
+    else if names_fn <> "" then
+      MyUtils.lines_of_file names_fn
+    else
+      assert(false) in
+  let name2rank =
+    let res = Ht.create 11 in
+    List.iteri (fun i name ->
+        assert(not (Ht.mem res name));
+        Ht.add res name i
+      ) selected_names;
+    res in
+  let nb_names = Ht.length name2rank in
+  let rank2mol = Ht.create nb_names in
+  let ok_names = StringSet.of_list selected_names in
   let count = ref 0 in
-  MyUtils.with_in_file !input_fn (fun input ->
+  MyUtils.with_in_file input_fn (fun input ->
       try
         while true do
           let m = Sdf.read_one input in
-          if List.exists (fun name ->
-              BatString.exists m name
-            ) names then
-            (Printf.printf "%s" m;
+          let name, _rest = BatString.split m ~by:"\n" in
+          if StringSet.mem name ok_names then
+            (if preserve_order then
+               let rank = Ht.find name2rank name in
+               Ht.add rank2mol rank m
+             else
+               Printf.printf "%s" m;
              incr count)
         done
       with End_of_file ->
-        Printf.eprintf "found %d\n" !count
+        (if preserve_order then
+           for rank = 0 to nb_names - 1; do
+             Printf.printf "%s" (Ht.find rank2mol rank)
+           done;
+         Printf.eprintf "found %d\n" !count)
     )
 
 let () = main ()
