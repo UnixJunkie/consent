fde711f9d08f5234245b7307b6ad012dab4491f0
Francois Berenger <unixjunkie@sdf.org>
2018-11-09 14:01:57 +0900

    sdf_get uses minicli and can get molecule names from a file
    instead of only directly on the command line

--- src/sdf_get.ml
+++ src/sdf_get.ml
@@ -9,30 +9,29 @@ module DB = Dokeysto_camltc.Db_camltc.RW
 let db_name_of fn =
   fn ^ ".db"
 
+type mol_name_provider = On_cli of string
+                       | From_file of string
+
 let main () =
   Log.set_log_level Log.INFO;
   Log.set_output stderr;
   Log.color_on ();
-  (* mandatory options *)
-  let input_fn = ref "" in
-  let names = ref "" in
-  let usage_message =
-    sprintf "usage:\n%s -i molecules.sdf -names \"mol1,mol2,mol10\""
-      Sys.argv.(0) in
-  let argc = Array.length Sys.argv in
+  let argc, args = CLI.init () in
   if argc = 1 then
-    (Log.fatal "%s" usage_message;
-     exit 1)
-  else
-    Arg.parse
-      ["-i", Arg.Set_string input_fn,
-       "<filename> where to read molecules from";
-       "-names", Arg.Set_string names,
-       "name1[,name2[,...]] which molecules to get"]
-      (fun arg -> raise (Arg.Bad ("Bad argument: " ^ arg)))
-      usage_message;
+    (eprintf "usage:\n\
+              %s -i molecules.sdf \
+              {-names \"mol1,mol2,...\"|-f names_file}\n"
+       Sys.argv.(0);
+     exit 1);
+  let input_fn = CLI.get_string ["-i"] args in
+  let names_provider =
+    match CLI.get_string_opt ["-names"] args with
+    | Some names -> On_cli names
+    | None ->
+      let fn = CLI.get_string ["-f"] args in
+      From_file fn in
   (* is there a DB already? *)
-  let db_fn = db_name_of !input_fn in
+  let db_fn = db_name_of input_fn in
   let db_exists, db =
     if Sys.file_exists db_fn then
       let () = Log.info "creating %s" db_fn in
@@ -42,7 +41,7 @@ let main () =
       (false, DB.create db_fn) in
   let count = ref 0 in
   if not db_exists then
-    MyUtils.with_in_file !input_fn (fun input ->
+    MyUtils.with_in_file input_fn (fun input ->
         try
           while true do
             let m = Sdf.read_one input in
@@ -55,7 +54,9 @@ let main () =
         with End_of_file ->
           DB.sync db
       );
-  let names = BatString.nsplit !names ~by:"," in
+  let names = match names_provider with
+    | On_cli names -> BatString.nsplit names ~by:","
+    | From_file fn -> MyUtils.lines_of_file fn in
   List.iter (fun name ->
       try
         let m = DB.find db name in
