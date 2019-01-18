1707ba2300cf1420c581c9d069008716f4c6b065
Francois Berenger <unixjunkie@sdf.org>
2019-01-18 16:58:02 +0900

    Smi: append '\n' at end of line
    as other file format readers do

--- src/smi.ml
+++ src/smi.ml
@@ -1,7 +1,7 @@
 
 (* read one molecule from a SMILES file *)
 let read_one (input: in_channel): string =
-  input_line input
+  (input_line input) ^ "\n"
 
 let get_name smiles_line =
   let _smiles, name = BatString.split smiles_line ~by:"\t" in
