f70dcc9ad016e76ca3b564eade4a703564bb3a0f
Francois Berenger <unixjunkie@sdf.org>
2019-02-06 14:29:55 +0900

    Smi: smartly fail in case smiles file is not tab separated

--- src/smi.ml
+++ src/smi.ml
@@ -4,7 +4,9 @@ let read_one (input: in_channel): string =
   (input_line input) ^ "\n"
 
 let get_name smiles_line =
-  let _smiles, name = BatString.split smiles_line ~by:"\t" in
+  let _smiles, name =
+    try BatString.split smiles_line ~by:"\t"
+    with Not_found -> failwith "Smi.get_name: smiles file not using tabs" in
   (* all other file formats expect molecule name only,
    * not molecule name followed by EOL *)
   if BatString.ends_with name "\n" then
