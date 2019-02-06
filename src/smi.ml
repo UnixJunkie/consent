1d40a1c0dae4ac37551ce0f13f2f0ed51043140d
Francois Berenger <unixjunkie@sdf.org>
2019-02-06 15:41:17 +0900

    Smi: robust against DOS-format smiles file

--- src/smi.ml
+++ src/smi.ml
@@ -1,7 +1,11 @@
 
 (* read one molecule from a SMILES file *)
 let read_one (input: in_channel): string =
-  (input_line input) ^ "\n"
+  let line = input_line input in
+  (* strip protects against trailing '\r'
+     we append '\n' because all other formats
+     end molecules with a '\n' *)
+  (BatString.strip line) ^ "\n"
 
 let get_name smiles_line =
   let _smiles, name =
@@ -9,7 +13,9 @@ let get_name smiles_line =
     with Not_found -> failwith "Smi.get_name: smiles file not using tabs" in
   (* all other file formats expect molecule name only,
    * not molecule name followed by EOL *)
-  if BatString.ends_with name "\n" then
+  if BatString.ends_with name "\r\n" then
+    BatString.rchop ~n:2 name
+  else if BatString.ends_with name "\n" then
     BatString.rchop ~n:1 name
   else
     name
